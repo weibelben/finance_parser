7423467e951e847aefdf4c582cfa55ca5639da0f
Ben Weibel <benw@extrahop.com>
2020-03-06 20:57:29 -0800

    Add basic go structure and citi files

--- cmd/citi.go
+++ cmd/citi.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	"github.com/weibelben/finance_parser/citibank"
+	"github.com/weibelben/finance_parser/pkg/citibank"
 )
 
 func main() {
 	log.Info("Your citibank data is being compiled...")
-	statements, err  := citibank.Find_citi_statements()
+	statements, err  := citibank.FindCitiStatements()
 	if err != nil {
 		log.WithError(err).Fatal("Failed to collect citibank statements.")
 	}
 
-	log.Info("%v", statements)
+	log.Info(statements)
 }
